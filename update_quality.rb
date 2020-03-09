a01463a4866e306ca8c228c8e796f6475ef4d2b2
Riley <riley.louvier@homes.com>
2020-03-09 13:09:36 -0400

    Updated ruby version and added first helper functions to break down monolith function

--- update_quality.rb
+++ update_quality.rb
@@ -1,4 +1,52 @@
-require 'award'
+require_relative 'award'
+
+
+def blue_plus_quality(award)
+  # it would appear this value never changes. Storing as static value and breaking monolithic function into several.
+  award.quality = 80
+end
+
+def blue_first_age(award)
+  # increases with 2 if expired and 1 otherwise
+  if award.expires_in <= 0
+    award.quality += 2
+  else
+    award.quality += 1
+  end
+ end
+
+def normal_age(award)
+  if award.expires_in <= 0
+    award.quality -= 2
+  else
+    award.quality -= 1
+  end
+end
+
+
+def blue_star_age(award)
+  if award.expires_in <= 0
+    award.quality -= 4
+  else
+    award.quality -= 2
+  end
+end
+
+
+def blue_quality_compare(award)
+  # if quality expires return 0
+  if award.expires_in <= 0
+    award.quality = 0
+  # +3 if it expires in 5 days
+  elsif award.expires_in <= 5
+    award.quality += 3
+  # +2 if it expires in 10 days
+  elsif award.expires_in <= 10
+    award.quality += 2
+  else
+    award.quality +=1
+  end
+end
 
 def update_quality(awards)
   awards.each do |award|
